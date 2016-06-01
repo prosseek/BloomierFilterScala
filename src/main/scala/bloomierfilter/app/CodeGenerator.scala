69fc718cc52068b232c5acab59791325086d07ae
smcho <sm.cho@mac.com>
2016-06-01 16:09:34 -0500

    [2016/06/01]04:09PM

--- src/main/scala/bloomierfilter/app/CodeGenerator.scala
+++ src/main/scala/bloomierfilter/app/CodeGenerator.scala
@@ -16,27 +16,33 @@ s"""
 if (key == "#{key}") {
   for (int i = 0; i < #{size}; i++) {
       int location[] = #{locations};
-      byteArray[#{start_location} + location[i]] = value[i];
+
+      for (int j = 0; j < #{Q}; j++) {
+          byteArray[#{start_location} + location[i] + j] = value[i * #{Q} + j];
+      }
   }
 }
 """
 
   def codeGen(information: Map[java.lang.String, Any]) = {
 
-    def getContent(key:java.lang.String, value:List[Int], startLocation:Int) = {
+    def getContent(key:java.lang.String, value:List[Int], startLocation:Int, Q:Int) = {
       val replacement = Map("key" -> key, "size" -> value.length.toString,
-        "locations" -> value.mkString("{",",","}"), "start_location" -> startLocation.toString)
+        "locations" -> value.mkString("{",",","}"),
+         "Q" -> Q.toString,
+        "start_location" -> startLocation.toString)
       getTemplateString(template, replacement)
     }
 
     val keyToList = information("keyToList").asInstanceOf[Map[String, List[Int]]]
     val startLocation = information("startLocation").asInstanceOf[Int]
+    val Q = information("Q").asInstanceOf[Int]
 
     val stringBuffer = new StringBuilder()
     stringBuffer ++= header
     keyToList foreach {
       case (key, value) => {
-        stringBuffer ++= getContent(key, value.asInstanceOf[List[Int]], startLocation)
+        stringBuffer ++= getContent(key, value.asInstanceOf[List[Int]], startLocation, Q)
       }
     }
     stringBuffer ++= tail
